1e350b7463283e4dc66f8a6b44980d33ade7b09f
Williams Hernandez <hernandw@gmail.com>
2022-04-12 10:47:21 -0400

    Updates

---
+++ precache-manifest.7a23a2923ef879bb480cc5023e18bb36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a5880ef9bd21067ed3a",
+    "url": "/mi-calculadora/static/css/main.cd4f6de5.chunk.css"
+  },
+  {
+    "revision": "6a5880ef9bd21067ed3a",
+    "url": "/mi-calculadora/static/js/main.6a5880ef.chunk.js"
+  },
+  {
+    "revision": "e2630ed4d8d20f1afdf7",
+    "url": "/mi-calculadora/static/js/1.e2630ed4.chunk.js"
+  },
+  {
+    "revision": "66812bd253ae5a72db48",
+    "url": "/mi-calculadora/static/js/runtime~main.66812bd2.js"
+  },
+  {
+    "revision": "5840836141fde3961a3f8a287f37ddb6",
+    "url": "/mi-calculadora/index.html"
+  }
+];
